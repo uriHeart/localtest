dc573799fb0890782073634186a646caa45f74dd
ags0688 <ags0688@gmail.com>
2019-08-28 00:08:27 +0900

    jpa 설정 추가

--- argo-common/src/main/java/com/argo/common/configuration/ArgoCommonConfig.java
+++ argo-common/src/main/java/com/argo/common/configuration/ArgoCommonConfig.java
@@ -1,20 +1,39 @@
 package com.argo.common.configuration;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.transaction.ChainedTransactionManager;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.annotation.Resource;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.argo.common.domain.common.jpa")
+@EnableJpaRepositories(basePackages = "com.argo")
 @EnableJpaAuditing
-@Import({CassandraConnectionConfiguration.class})
-@EntityScan(basePackages = "com.argo.common")
+@Import({ArgoPostgreSqlConfig.class, CassandraConnectionConfiguration.class})
+@EntityScan(basePackages = "com.argo")
 @EnableAsync(proxyTargetClass = true)
 @ComponentScan(basePackages = {"com.argo"})
-public class ArgoCommonConfig implements AsyncConfigurer {
+public class ArgoCommonConfig implements AsyncConfigurer, TransactionManagementConfigurer {
+
+    @Resource
+    private PlatformTransactionManager transactionManager;
+
+    @Bean
+    public PlatformTransactionManager chainedTransactionManager() {
+        return new ChainedTransactionManager(transactionManager);
+    }
+
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return chainedTransactionManager();
+    }
 }
