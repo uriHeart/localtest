4d4e92b62c4906ddb7b39a3ef84b37f5cfede08f
uriHeart <karismmm@gmail.com>
2019-09-20 00:01:08 +0900

    엑셀 업로드 및 조회

--- argo-collector/src/main/java/com/argo/collect/service/ConvertService.java
+++ argo-collector/src/main/java/com/argo/collect/service/ConvertService.java
@@ -3,10 +3,34 @@ package com.argo.collect.service;
 import com.argo.common.domain.raw.RawEvent;
 import com.argo.common.domain.raw.RawEventService;
 import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.IndicesOptions;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.core.CountRequest;
+import org.elasticsearch.client.core.CountResponse;
+import org.elasticsearch.client.indices.GetMappingsRequest;
+import org.elasticsearch.client.indices.GetMappingsResponse;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.FieldSortBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -18,42 +42,67 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Service
+@Slf4j
 public class ConvertService {
 
     @Value("${file.upload.default-path}")
     private String path;
 
+    @Value("${file.upload.index}")
+    private String excelIndex;
+
     @Autowired
     RawEventService rawEventService;
+
+
+    @Autowired
+    Gson gson;
+
+    private RestHighLevelClient esClient;
+
+    @Autowired
+    public ConvertService(RestHighLevelClient client) {
+        this.esClient = client;
+    }
 
     public File excelUpload(MultipartFile filePart) throws IOException {
         String sourceFileName = filePart.getOriginalFilename();
         String sourceFileNameExtension = FilenameUtils.getExtension(sourceFileName).toLowerCase();
+        String sourceFileBaseName = FilenameUtils.getBaseName(sourceFileName);
         File destinationFile;
-        String destinationFileName;
+        // String destinationFileName;
 
         //S3에 저장한다면 파일명 변환은 필요 없음
-        do {
-            destinationFileName = RandomStringUtils.randomAlphanumeric(32) + "." + sourceFileNameExtension;
-            destinationFile = new File(path+"/" + destinationFileName);
-        } while (destinationFile.exists());
+//        do {
+//            //destinationFileName = RandomStringUtils.randomAlphanumeric(32) + "." + sourceFileNameExtension;
+//            destinationFile = new File(path+"/" + destinationFileName);
+//        } while (destinationFile.exists());
+
+        destinationFile = new File(path+"/" + sourceFileName);
+
+        if(destinationFile.exists()){
+            String milliSecondLong = String.valueOf(Instant.now().getEpochSecond());
+            destinationFile = new File(path+"/" + sourceFileBaseName+"_"+milliSecondLong + "."+sourceFileNameExtension);
+        }
+
         destinationFile.getParentFile().mkdirs();
         filePart.transferTo(destinationFile);
 
         return destinationFile;
     }
 
-    public List<List<HashMap<String,String>>> excelToJson(File file)
+    public List<HashMap<String,Object>> excelToJson(File file)
     {
-        List<List<HashMap<String,String>>> result = new ArrayList<>();
+        List<HashMap<String,Object>> result = new ArrayList<>();
+
         try{
             FileInputStream fInputStream = new FileInputStream(file);
 
@@ -71,8 +120,14 @@ public class ConvertService {
                 {
 
                     List<List<String>> sheetDataTable = getSheetDataList(sheet);
+                    List<List<String>> mainDataTable = getMainDataList(sheetDataTable);
 
-                    result.add(getJSONStringFromList(sheetDataTable));
+                    HashMap<String,Object> convertSheet = new HashMap<>();
+
+                    convertSheet.put("sheetHeader",mainDataTable.get(0));
+                    convertSheet.put("sheetName",sheetName);
+                    convertSheet.put("sheetData",getJSONStringFromList(mainDataTable));
+                    result.add(convertSheet);
 
                 }
             }
@@ -102,6 +157,7 @@ public class ConvertService {
                 int firstCellNum = row.getFirstCellNum();
                 int lastCellNum = row.getLastCellNum();
 
+
                 List<String> rowDataList = new ArrayList<String>();
 
                 for(int j = firstCellNum; j < lastCellNum; j++)
@@ -116,9 +172,16 @@ public class ConvertService {
                         rowDataList.add(dateString);
                     }else if(cellType == CellType.NUMERIC)
                     {
-                        double numberValue = cell.getNumericCellValue();
+                        Double doubleValue = cell.getNumericCellValue();
 
-                        String stringCellValue = BigDecimal.valueOf(numberValue).toPlainString();
+                        String value = "";
+                        if(doubleValue >0){
+                            value = String.valueOf(Math.abs(doubleValue*100/100.0));
+                        } else{
+                            value = String.valueOf(Math.round(doubleValue*100/100.0));
+                        }
+
+                        String stringCellValue = value;
 
                         rowDataList.add(stringCellValue);
 
@@ -134,6 +197,14 @@ public class ConvertService {
 
                         rowDataList.add(stringCellValue);
 
+                    }else if(cellType == CellType.FORMULA)
+                    {
+
+                        long value = Math.round(cell.getNumericCellValue());
+                        String stringCellValue = String.valueOf(value);
+
+                        rowDataList.add(stringCellValue);
+
                     }else if(cellType == CellType.BLANK)
                     {
                         rowDataList.add("");
@@ -143,9 +214,41 @@ public class ConvertService {
                 ret.add(rowDataList);
             }
         }
+
         return ret;
     }
 
+    //엑셀의 불필요한 데이터 제거
+    private List<List<String>> getMainDataList(List<List<String>> sheetDataList){
+
+        int avgCellCount = (int) sheetDataList.stream().mapToInt(List::size).average().orElse(0);
+
+
+        List<List<String>> temp = new ArrayList<>();
+        List<List<String>> mainDataList = new ArrayList<>();
+
+        //row의 cell 데이터가 평균 이상이여야 등록한다. 제목 합계등 만 있는 row는 제외
+        //병균 셀데이터의 1/3 보다 많은 공백값이 있는 데이터 제외
+        sheetDataList.forEach(row ->{
+            if(row.size() >= avgCellCount && blankCount(row) <= avgCellCount/3){
+                temp.add(row);
+            }
+        });
+
+        boolean findHeader = false;
+
+        //셀을 병합하여 공백값만 있는 row의 헤더 이전 row 제외
+        for (List<String> row : temp) {
+            if(findHeader || isHeader(row) ){
+                findHeader = true;
+                mainDataList.add(row);
+            }
+        };
+
+        return mainDataList;
+    }
+
+    // 헤더 value : cell 값 의 형태의 데이터를 만든다.
     private List<HashMap<String,String>> getJSONStringFromList(List<List<String>> dataTable){
 
         List<HashMap<String,String>> result = new ArrayList<>();
@@ -163,7 +266,7 @@ public class ConvertService {
                 {
                     List<String> dataRow = dataTable.get(i);
 
-                    HashMap<String,String> col = new HashMap();
+                    LinkedHashMap<String,String> col = new LinkedHashMap();
 
                     for(int j=0;j<columnCount;j++)
                     {
@@ -172,39 +275,124 @@ public class ConvertService {
 
                         col.put(columnName, columnValue);
                     }
-                       result.add(col);
+                    result.add(col);
                 }
             }
         }
         return result;
     }
 
-    public void saveToCassandra(List<List<HashMap<String,String>>> jsonData, Long channelId, Long vendorId) {
-        Gson gson = new Gson();
-        //sheet 단위 채널단위가 되지 않을까?
-        jsonData.forEach(jsonObjects -> {
-            //record 단위
-            jsonObjects.forEach(jsonRaw ->{
-                SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                Date orderDate = null;
-                try {
-                     orderDate = transFormat.parse(jsonRaw.get("order_date"));
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                    orderDate = new Date();
-                }
-                RawEvent rawEvent = RawEvent.builder()
-                        .vendorId(vendorId)
-                        .channelId(channelId)
-                        .format("JSON")
-                        .auto(false)
-                        .data(gson.toJson(jsonRaw))
-                        .orderId(jsonRaw.get("oder_no"))
-                        .publishedAt(orderDate)
-                        .createdAt(new Date())
-                        .build();
-                rawEventService.save(rawEvent);
-            });
+    //헤더의 모든값은 공백문자가 아니여야 한다.
+    private boolean isHeader(List<String> row){
+
+        AtomicBoolean isHeader = new AtomicBoolean(true);
+
+        row.forEach( cell ->{
+            if(cell.length()==0){
+                isHeader.set(false);
+            }
         });
+
+        return isHeader.get();
+    }
+
+    //공백문자의수 를 확인한다.
+    private long blankCount(List<String> row){
+        return row.stream().filter(cell->(cell.length()==0)).count();
+    }
+
+
+    public void saveToCassandra(List<HashMap<String,List<HashMap<String,String>>>> jsonData, Long channelId, Long vendorId) {
+        //sheet 단위 채널단위가 되지 않을까?
+        jsonData.forEach(sheet -> {
+            sheet.values().forEach(jsonObjets ->{
+                //record 단위
+
+                jsonObjets.forEach(jsonRaw ->{
+                    SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    Date orderDate = null;
+                    try {
+                        orderDate = transFormat.parse(jsonRaw.get("order_date"));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                        orderDate = new Date();
+                    }
+                    RawEvent rawEvent = RawEvent.builder()
+                            .vendorId(vendorId)
+                            .channelId(channelId)
+                            .format("JSON")
+                            .auto(false)
+                            .data(gson.toJson(jsonRaw))
+                            .orderId(jsonRaw.get("oder_no"))
+                            .publishedAt(orderDate)
+                            .createdAt(new Date())
+                            .build();
+                    rawEventService.save(rawEvent);
+                });
+            });
+
+        });
+    }
+
+    public RestStatus saveToEs(String id, List<HashMap<String, Object>> jsonData) throws IOException {
+
+        String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+        IndexRequest request = new IndexRequest(excelIndex);
+
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("upLoadDate",dateTime);
+        data.put("@timeStamp",Instant.now().getEpochSecond());
+        data.put("excelData",jsonData);
+        data.put("fileName",id);
+
+        request.source(gson.toJson(data), XContentType.JSON);
+
+        IndexResponse response = esClient.index(request, RequestOptions.DEFAULT);
+        CountRequest countRequest = new CountRequest(excelIndex);
+        CountResponse countResponse =  esClient.count(countRequest, RequestOptions.DEFAULT);
+        log.info(String.valueOf(countResponse.getCount()));
+        return response.status();
+    }
+
+    public List<Map<String,Object>> getExcelList(String userId) throws IOException {
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        QueryBuilder matchAll = QueryBuilders.matchAllQuery();
+
+        //TODO 사용자 ID에 해당하는 데이터 조회
+        searchSourceBuilder.query(matchAll);
+
+        searchSourceBuilder.size(1000);
+        searchSourceBuilder.fetchSource(new String[]{"upLoadDate", "fileName"},new String[]{""});
+        searchSourceBuilder.sort(new FieldSortBuilder("@timeStamp").order(SortOrder.DESC));
+
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices(excelIndex);
+        searchRequest.source(searchSourceBuilder);
+
+        SearchResponse response = esClient.search(searchRequest, RequestOptions.DEFAULT );
+        SearchHits excelList =  response.getHits();
+
+        List<Map<String,Object>> result = new ArrayList<>();
+
+        excelList.forEach(
+                excel ->{
+                    Map<String, Object> excelInfo = excel.getSourceAsMap();
+                    excelInfo.put("id",excel.getId());
+                    result.add(excelInfo);
+                }
+        );
+
+        return result;
+    }
+
+    public ArrayList<HashMap<String,HashMap<String,Object>>> getExcelMainText(String indexId) throws IOException {
+        GetRequest getRequest = new GetRequest(excelIndex,indexId);
+        GetResponse getResponse = esClient.get(getRequest, RequestOptions.DEFAULT);
+        ArrayList<HashMap<String,HashMap<String,Object>>> data = (ArrayList<HashMap<String, HashMap<String, Object>>>) getResponse.getSourceAsMap().get("excelData");
+
+        return data;
     }
 }
--- argo-collector/src/main/java/com/argo/collect/web/ConvertController.java
+++ argo-collector/src/main/java/com/argo/collect/web/ConvertController.java
@@ -1,46 +1,87 @@
 package com.argo.collect.web;
 
-
 import com.argo.collect.service.ConvertService;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.rest.RestStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*")
 @RestController
+@Slf4j
 public class ConvertController {
 
     @Autowired
     ConvertService convertService;
 
     @RequestMapping(value="/excelUpload",
-                    method = RequestMethod.POST,
-                    consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+            method = RequestMethod.POST,
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> excelUpload(@RequestParam("file")MultipartFile parts,
-                                      @RequestParam("channelId")Long channelId,
-                                      @RequestParam("vendorId")Long vendorId)throws IOException {
-        //엑셀저장
-        File excel = convertService.excelUpload(parts);
+                                              @RequestParam("channelId")Long channelId,
+                                              @RequestParam("vendorId")Long vendorId){
+        String resultMessage = "";
+        HttpStatus status = HttpStatus.OK;
 
-        //엑셀데이터 json변환  List<엑셀sheet List<레코드 >
-        List<List<HashMap<String,String>>> jsonData = convertService.excelToJson(excel);
+        try {
+            //엑셀저장
+            File excel = convertService.excelUpload(parts);
 
-        //TODO
-        //엑셀 헤더를 저장할데이터의 key값으로 맵핑 설정하는 기능 ex)주문번호 =>oderNo
+            //엑셀데이터 json변환  List<엑셀sheet List<레코드 >
+            List<HashMap<String, Object>> jsonData = convertService.excelToJson(excel);
 
-        //카산드라에 raw데이터 저장
-        convertService.saveToCassandra(jsonData,channelId,vendorId);
+            //현재 동기호출이 안됨.
+            RestStatus restStatus = convertService.saveToEs(excel.getName(), jsonData);
 
+            //카산드라에 raw데이터 저장
+            resultMessage = restStatus.name();
+            //convertService.saveToCassandra(jsonData,channelId,vendorId);
+        }catch (Exception e){
+            resultMessage = "upload fail!";
+            status = HttpStatus.SERVICE_UNAVAILABLE;
+        }
 
-        return new ResponseEntity<String>("upload success", HttpStatus.OK);
+        return new ResponseEntity<>(resultMessage, status);
+    }
+
+    @RequestMapping(value="/excel/list",
+            method = RequestMethod.GET,
+            produces = "application/json; charset=utf8")
+    public @ResponseBody List<Map<String,Object>> getUploadList(HttpServletRequest req) throws IOException {
+        //TODO userId에 해당하는 업로드파일만 조회
+        //HttpSession session = req.getSession();
+        //String userId = (String)session.getAttribute("userId");
+        List<Map<String,Object>> excelList = convertService.getExcelList("");
+        return excelList;
+    }
+
+    /**
+     *TODO
+     * 업로드한 엑셀의 헤더와 본문내용을 가져와 front의 그리드에 출력한다.
+     * 별도로 엑셀 문서마다 헤더 맵핑을 하지않고 업로드한 엑셀 형식을 동적으로 보여준다.
+     * 차후 저장 할 엑셀문서의 양식이 확정되면 헤더맵핑한다.
+     *
+     * @param indexId
+     * @return Object
+     * @throws IOException
+     */
+    @RequestMapping(value="/excel/detail",
+            method = RequestMethod.GET,
+            produces = "application/json; charset=utf8")
+    public @ResponseBody Object getExcelMainText(@RequestParam String indexId) throws IOException {
+        ArrayList<HashMap<String, HashMap<String, Object>>> excelMainText = convertService.getExcelMainText(indexId);
+        return excelMainText;
     }
 
 }
