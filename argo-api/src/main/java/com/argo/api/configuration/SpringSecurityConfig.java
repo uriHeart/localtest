f5691fb73aa58589fc1e23a043bd337bbc51e7d8
ags0688 <ags0688@gmail.com>
2019-11-15 23:25:24 +0900

    security 수정

--- argo-api/src/main/java/com/argo/api/configuration/SpringSecurityConfig.java
+++ argo-api/src/main/java/com/argo/api/configuration/SpringSecurityConfig.java
@@ -27,7 +27,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .authorizeRequests()
                 .antMatchers("/error**", "/api/auth/**").permitAll()
-                .antMatchers("/**", "/api/check/auth").authenticated()
+                .antMatchers("/**").authenticated()
                 .antMatchers("/admin/**").access(RoleType.ADMIN.name())
             .and()
                 .formLogin().disable()
