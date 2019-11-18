d95c288136796dd41266a9e649b42eb380579429
ags0688 <ags0688@gmail.com>
2019-11-18 21:32:50 +0900

    collector buildspec 정의

---
+++ config/argo-collector-before.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/ec2-user/argo-collector
+rm -rf *
---
+++ config/argo-collector-start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /home/ec2-user/argo-collector
+ls
+sudo chmod +x argo-collector-0.0.1-SNAPSHOT.jar
+sudo service argoCollector start
