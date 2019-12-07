4c865d06078437d8da2be1fe4497624e900405da
uriHeart <karismmm@gmail.com>
2019-12-07 10:24:00 +0900

    Merge branch 'master' of https://github.com/uriHeart/localtest
    # Conflicts:
    #	argo-api/src/main/resources/application.yml
    #	argo-collector/src/test/java/com/argo/collect/domain/collector/SsgOrderCollectorTest.java

--- argo-common/src/main/java/com/argo/common/domain/common/jpa/EventType.java
+++ argo-common/src/main/java/com/argo/common/domain/common/jpa/EventType.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 public enum EventType {
     ORDER("ORDER"), //주문
     CANCEL("CANCEL"), //주문취소
+    RELEASE_REQUEST("RELEASE"),//출고
     RELEASE("RELEASE"),//출고
+    DELIVERY("DELIVERY"),//배송
+    DELIVERY_END("DELIVERY_END"),//배송완료
     EXCHANGE("EXCHANGE"), //교환
     RETURN("RETURN"), //환불
     OTHER("OTHER");
